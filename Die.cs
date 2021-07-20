b1f359899142f9fd3dcd9dfe54dd49e955d3270d
Timothy Fox <timothyfox443@gmail.com>
2021-07-19 21:23:34 -0700

    changed Die constructor to include static fields for upper/lower bounds for varying die values

--- Die.cs
+++ Die.cs
@@ -7,14 +7,51 @@ using System.Threading.Tasks;
 namespace EncasulationDemoWithDieClass
 {
     /// <summary>
-    /// this will represent a single die
+    /// this will represent a single multi-sided die
+    /// at this time
     /// </summary>
     class Die
     {
+        static Random rand;
+        byte minValue;
+        byte maxValue;
+  //shared across all instances of the class
+        static Die()
+        {
+            rand = new Random();
+        }
+        public Die() : this(1,6)
+        {
+            //minValue = 1;
+            //maxValue = 6;
+            //Roll(); //this prevents an illegal value being stored in a die
+        }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="minValue">the inclusive lower value</param>
+    /// <param name="maxValue">the exclusive upper value needs +1 more than highest value desired</param>
+    public Die(byte minValue, byte maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            Roll();
+        }
         public byte Value { get; private set; }
 
         public bool IsHeld { get; set; }
 
         //roll method here
+        /// <summary>
+        /// rolls a six sided die and returns the value
+        /// </summary>
+        public byte Roll()
+        {
+            if (!IsHeld) // if this die is not held do this
+            {
+                Value = (byte)rand.Next(minValue,maxValue);
+            }
+            return Value;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ namespace EncasulationDemoWithDieClass
     {
         static void Main(string[] args)
         {
-            Die die1 = new Die();
-            //die1.Value =  no can do
+            Die[] dice = new Die[5];
+            for (int i = 0; i < dice.Length; i++)
+            {
+                dice[i] = new Die();
+                Console.WriteLine(dice[i].Value);
+
+            }
             
-            Console.WriteLine("Hello World!");
-        }
+/*            Die die1 = new Die();
+            //die1.Value =  cannot set value outside class
+            Console.WriteLine(die1.Value);
+            die1.Roll();
+            Console.WriteLine(die1.Value);
+            Console.ReadKey();
+*/        }
     }
 }
