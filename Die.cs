3184471843f11d6d853aab6b357aacdd62ecf890
Timothy Fox <timothyfox443@gmail.com>
2021-07-19 19:52:28 -0700

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncasulationDemoWithDieClass
+{
+    /// <summary>
+    /// this will represent a single die
+    /// </summary>
+    class Die
+    {
+        public byte Value { get; private set; }
+
+        public bool IsHeld { get; set; }
+
+        //roll method here
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EncasulationDemoWithDieClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            //die1.Value =  no can do
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
